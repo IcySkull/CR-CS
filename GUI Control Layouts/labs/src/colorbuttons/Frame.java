dbd6a45f4ad661b833ee47e46365e612b173a608
Doblegor <diejorarr@gmail.com>
2022-01-16 21:36:59 -0600

    "Resizable buttons implemented"

--- GUI Control Layouts/labs/src/colorbuttons/Frame.java
+++ GUI Control Layouts/labs/src/colorbuttons/Frame.java
@@ -2,12 +2,17 @@ package colorbuttons;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ComponentEvent;
+import java.awt.event.ComponentListener;
+import java.awt.List;
+import java.util.ArrayList;
 
 public class Frame extends JFrame {
     public static void main(String args[]) {
         new Frame();
     }
 
+    static ArrayList<ColorButton> colorButtons = new ArrayList<>();
     ColorButton redB = new ColorButton(Color.red, "RED");
     ColorButton greenB = new ColorButton(Color.green, "GREEN");
     ColorButton blueB = new ColorButton(Color.blue, "BlUE");
@@ -20,6 +25,33 @@ public class Frame extends JFrame {
         setVisible(true);
         setPreferredSize(new Dimension(500, 500));
         pack();
+
+        this.addComponentListener(new ComponentListener() {
+            @Override
+            public void componentResized(ComponentEvent e) {
+                for (ColorButton b : colorButtons) {
+                    float fontSize = (float)(Math.min(getWidth()*0.07, getHeight()*0.07));
+                    b.setPreferredSize(new Dimension((int)(Math.round(getWidth()*0.35)),
+                            (int)(Math.round(getHeight()*0.25))));
+                    b.setFont(b.getFont().deriveFont(fontSize));
+                }
+            }
+
+            @Override
+            public void componentMoved(ComponentEvent e) {
+
+            }
+
+            @Override
+            public void componentShown(ComponentEvent e) {
+
+            }
+
+            @Override
+            public void componentHidden(ComponentEvent e) {
+
+            }
+        });
 
         redB.addActionListener(ae -> getContentPane().setBackground(redB.color));
         greenB.addActionListener(ae -> getContentPane().setBackground(greenB.color));
@@ -42,6 +74,7 @@ public class Frame extends JFrame {
             this.color = color;
             setPreferredSize(new Dimension(80, 50));
             setBackground(this.color);
+            colorButtons.add(this);
         }
     }
 }
