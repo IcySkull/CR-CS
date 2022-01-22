74fa94e9df8e34f114adece97eae99ed972ecf96
Doblegor <diejorarr@gmail.com>
2022-01-22 17:41:05 -0600

    "suing visual code"

---
+++ GUI Control Layouts/labs/src/practices/BasicApp.java
@@ -0,0 +1,34 @@
+package practices;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class BasicApp extends JFrame {
+     public static void main(String[] args) {
+         JFrame frame = new BasicApp();
+     }
+
+     ContentPanel pane = new ContentPanel();
+
+     BasicApp() {
+         super("test");
+         setVisible(true);
+         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+         setLayout(new GridBagLayout());
+
+//         Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
+//         setBounds(
+//                 (int)(Math.round(.25 * (screen.width))),
+//                 (int)(Math.round(.25 * (screen.height))),
+//                 (int)(Math.round(.5 * (screen.width))),
+//                 (int)(Math.round(.5 * (screen.height)))
+//         );
+
+         GridBagConstraints gbc = new GridBagConstraints();
+         gbc.insets = new Insets(50, 50, 50, 50);
+
+         setContentPane(pane);
+         pack();
+     }
+}
+
--- GUI Control Layouts/labs/src/practices/ContentPanel.java
+++ GUI Control Layouts/labs/src/practices/ContentPanel.java
@@ -1,2 +1,22 @@
-package practices;public class ContentPanel {
+package practices;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class ContentPanel extends JPanel {
+    DrawingPanel board = new DrawingPanel();
+
+    ContentPanel() {
+        setLayout(new GridBagLayout());
+        setBackground(Color.gray);
+
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.insets = new Insets(50, 50, 50, 50);
+        add(board);
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+    }
 }
