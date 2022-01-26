9522d7ed76c5fcba375fc1603d67644d737c5694
Doblegor <diejorarr@gmail.com>
2022-01-25 19:39:12 -0600

    "PaintUser class added"

--- GUI Control Layouts/labs/src/paintproject/PaintApp.java
+++ GUI Control Layouts/labs/src/paintproject/PaintApp.java
@@ -2,8 +2,10 @@ package paintproject;
 
 import javax.swing.*;
 import javax.swing.plaf.basic.BasicButtonUI;
+import javax.tools.Tool;
 import java.awt.*;
 import java.awt.event.*;
+import java.util.Arrays;
 
 
 /* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -15,13 +17,17 @@ public class PaintApp extends JFrame {
     }
 
     // Instance variables
-    ToolsPanel toolsPanel = new ToolsPanel();
+    PaintUser user = new PaintUser();
+    ToolsComponent toolsPanel = new ToolsComponent();
     TabbedTables tables = new TabbedTables();
+    GridBagConstraints gbc;
 
     PaintApp() {
+        setTitle("Paint");
         setGridBagLayout(new GridBagLayout());
         setVisible(true);
-        setDimension(1200, 600);
+        setConstrains();
+        setDimension(1200, 650);
         setBackground(Color.white);
 
         addComponents();
@@ -38,6 +44,11 @@ public class PaintApp extends JFrame {
         setPreferredSize(size);
     }
 
+    public void setConstrains() {
+        gbc = new GridBagConstraints();
+        gbc.fill = GridBagConstraints.BOTH;
+    }
+
     // set the Table component to occupy all the space disposable and the Tools component to a fixed width space
     public void setGridBagLayout(GridBagLayout gbl) {
         setLayout(gbl);
@@ -46,11 +57,8 @@ public class PaintApp extends JFrame {
         gbl.columnWidths = new int[]{200}; // fixed width space for the first column
     }
 
-
     public void addComponents() {
         initMenu();
-        GridBagConstraints gbc = new GridBagConstraints();
-        gbc.fill = GridBagConstraints.BOTH;
 
         // add tools panels
         gbc.gridx = 0;
@@ -66,26 +74,26 @@ public class PaintApp extends JFrame {
      */
     public void initMenu() {
         JMenuBar menuBar = new JMenuBar();
-        JMenu menuFile = new JMenu("FILE");
-        JMenu menuView = new JMenu("VIEW");
+        JMenu menuFile = new JMenu("File");
+        JMenu menuView = new JMenu("View");
 
         // menuFile items
-        JMenuItem addFile = new JMenuItem("New Draw");
+        JMenuItem addFile = new JMenuItem("New draw");
         addFile.addActionListener(e -> {
             String title = JOptionPane.showInputDialog(
                     null,
                     "Enter the name of the draw",
-                    "New Draw",
+                    "New draw",
                     JOptionPane.INFORMATION_MESSAGE);
             String width = JOptionPane.showInputDialog(
                     null,
                     "Enter the width of the draw",
-                    "New Draw",
+                    "New draw",
                     JOptionPane.INFORMATION_MESSAGE);
             String height = JOptionPane.showInputDialog(
                     null,
                     "Enter the height of the draw",
-                    "New Draw",
+                    "New draw",
                     JOptionPane.INFORMATION_MESSAGE);
 
             int widthNum = Integer.parseInt(width);
@@ -95,7 +103,7 @@ public class PaintApp extends JFrame {
         });
 
         // menuView items
-        JRadioButtonMenuItem tools = new JRadioButtonMenuItem("Tools");
+        JRadioButtonMenuItem tools = new JRadioButtonMenuItem("Editor");
         tools.addChangeListener(e -> {
 
         });
@@ -111,11 +119,54 @@ public class PaintApp extends JFrame {
         setJMenuBar(menuBar);
     }
 
+    /**
+     * Centers the frame of the PaintApp
+     */
     public void centerFrame() {
         Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
         int x = (int) (Math.round((screenSize.getWidth() - getWidth()) / 2));
         int y = (int) (Math.round((screenSize.getHeight() - getHeight()) / 2));
         setLocation(x, y);
+    }
+
+    /* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        A formalized definition of a user of the app. It will provide accessible
+        information of the user so the App will interact with these variables to
+        improve the experience of the user.
+     */
+    public class PaintUser {
+        //Instance variables
+        Color color;
+        int stroke;
+
+        PaintUser() {
+        color = Color.white;
+        stroke = 12;
+        }
+
+        public void setColor(Color color) {
+            this.color = color;
+        }
+
+        public void setStroke(int stroke) {
+            this.stroke = stroke;
+        }
+
+        public Color getColor() {
+            return color;
+        }
+
+        public int getStroke() {
+            return stroke;
+        }
+
+        @Override
+        public String toString() {
+            return "PaintUser{" +
+                    "color=" + color +
+                    ", stroke=" + stroke +
+                    '}';
+        }
     }
 
     /* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -124,10 +175,6 @@ public class PaintApp extends JFrame {
     class TabbedTables extends JTabbedPane {
         // Instance variables
         int index;
-
-        // Default constructor of the class
-        TabbedTables() {
-        }
 
         public void setGridBagLayout(GridBagLayout gbl) {
             setLayout(gbl);
@@ -145,7 +192,8 @@ public class PaintApp extends JFrame {
         }
 
         /* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-            The component that will be used as the tab
+            The component that will be used as the tab. This class was not created by me.
+            It was implemented from an official solution of oracle.
          */
         public class ButtonTabComponent extends JPanel {
             private final JTabbedPane pane;
@@ -266,6 +314,8 @@ public class PaintApp extends JFrame {
     public class Table extends JPanel {
         // Instance Variables
         int width, height; // note that these are the dimensions of the Board
+        Color userColor;
+        int userStroke;
 
         // Default constructor of Table class
         Table() {
@@ -277,6 +327,7 @@ public class PaintApp extends JFrame {
             setBackground(new Color(150, 150, 150));
         }
 
+        // Construct a table with the specific width and height for the Board object
         Table(int width, int height) {
             this.width = width;
             this.height = height;
@@ -304,23 +355,29 @@ public class PaintApp extends JFrame {
         /* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
             Inner class of Table. The Board class will provide the base layout for the drawings.
          */
-        public class Board extends JPanel {
-            // Instance variables
-            int width, height;
+        public class Board extends JLayeredPane {
+            int index; // index to keep track of the layers created
+            GridBagConstraints gbc;
 
-            Board(Table table) {
+            Board(Table table) { // there must be a Table object to create a board
                 setGridBagLayout(new GridBagLayout());
+                setConstrains();
                 setVisible(true);
-                setBackground(Color.WHITE);
                 width = table.width;
                 height = table.height;
-                addLayer();
+                addLayer(); // base layer of the initialization of a board
             }
 
+            /**
+             * Add a new layer at the current index to this Board object.
+             */
             public void addLayer() {
-                GridBagConstraints gbc = new GridBagConstraints();
+                add(new Layer(index), index++);
+            }
+
+            public void setConstrains() {
+                gbc = new GridBagConstraints();
                 gbc.fill = GridBagConstraints.BOTH;
-                add(new Layers(), gbc);
             }
 
             public void setGridBagLayout(GridBagLayout gbl) {
@@ -329,17 +386,37 @@ public class PaintApp extends JFrame {
                 gbl.columnWeights = new double[]{1};
             }
 
+
+
             /* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
                 Inner class of Board. All the layers in conjunction will provide to the user the tool of swap
                 layers
              */
-            public class Layers extends JLayeredPane {
+            public class Layer extends JLayeredPane {
+                GridBagConstraints gbc;
                 int index;
+                String identifier;
 
-                Layers() {
+                Layer(int index) {
                     setGridBagLayout(new GridBagLayout());
                     setVisible(true);
-                    addDraw(new Draw());
+                    setConstrains();
+                    addDraw();
+                    this.index = index;
+                    identifier = Integer.toString(index);
+                }
+
+                Layer(int index, String identifier) {
+                    setGridBagLayout(new GridBagLayout());
+                    setVisible(true);
+                    addDraw();
+                    this.index = index;
+                    this.identifier = identifier;
+                }
+
+                public void setConstrains() {
+                    gbc = new GridBagConstraints();
+                    gbc.fill = GridBagConstraints.BOTH;
                 }
 
                 public void setGridBagLayout(GridBagLayout gbl) {
@@ -348,10 +425,8 @@ public class PaintApp extends JFrame {
                     gbl.columnWeights = new double[]{1};
                 }
 
-                public void addDraw(Draw draw) {
-                    GridBagConstraints gbc = new GridBagConstraints();
-                    gbc.fill = GridBagConstraints.BOTH;
-                    add(draw, gbc);
+                public void addDraw() {
+                    add(new Draw(), index++);
                 }
 
                 /* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -359,7 +434,7 @@ public class PaintApp extends JFrame {
                  */
                 public class Draw extends JPanel {
                     Draw() {
-                        setVisible(true);
+                        setOpaque(false);
                     }
 
                     @Override
@@ -382,25 +457,115 @@ public class PaintApp extends JFrame {
      ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
 
     /* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-            Tools that will be used for the user to interact with the Table class
+            The component that will be used to create the Tools whose functionalities
+            will provide to the user the ability to interact with the Board objects.
     */
-    public class ToolsPanel extends JPanel {
-        ToolsPanel() {
+    public class ToolsComponent extends JPanel {
+        GridBagConstraints gbc;
+        GridBagLayout gbl;
+        int index; // int to keep track of the tools created in the panel
+        final int TOOL_HEIGHT = 150;
+
+        // Default constructor of ToolsPanel
+        ToolsComponent() {
             setGridBagLayout(new GridBagLayout());
             setVisible(true);
             setBackground(new Color(200, 200, 200));
+            setConstrains();
+            setBorder(BorderFactory.createLoweredBevelBorder());
+            index = 0;
+
+        }
+
+        /**
+         * Add the ToolPanel provided to the body of the ToolComponent object.
+         * @param tool note that this Object most of the time is a child of the ToolPanel class
+         */
+        public void addTool(ToolPanel tool) {
+            changeIndex(true);
+            gbc.gridy = index-1;
+            add(tool, gbc);
+        }
+
+        /**
+         * Remove the ToolPanel provided from the body of the object ToolsComponent
+         * @param tool this object can -most probably will be- a child of ToolPanel
+         */
+        public void removeTool(ToolPanel tool) {
+            changeIndex(false);
+            remove(tool);
+        }
+
+        /**
+         *  Increment or decrement the index of the tools created, therefore the
+         *  rowHeight is augmented due its dependency when aligning the ToolPanel objects.
+         */
+        public void changeIndex(boolean increment) {
+            int sign = increment ?  1 : -1;
+            this.index += sign;
+            int[] rwHeight = new int[this.index];
+            Arrays.fill(rwHeight, TOOL_HEIGHT);
+            System.out.println(Arrays.toString(rwHeight));
+            gbl.rowHeights = rwHeight;
+        }
+
+        public void setConstrains() {
+            gbc = new GridBagConstraints();
+            gbc.fill = GridBagConstraints.BOTH;
+            gbc.insets = new Insets(5, 5, 5, 5);
         }
 
         public void setGridBagLayout(GridBagLayout gbl) {
             setLayout(gbl);
+            gbl.columnWeights = new double[]{1};
+            this.gbl = gbl;
         }
+
+        /* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+            The JPanel object that will provide the blueprint to create every tool.
+        */
+        public class ToolPanel extends JPanel {
+            GridBagLayout gbl;
+            GridBagConstraints gbc;
+
+            ToolPanel() {
+                setGridBagLayout(new GridBagLayout());
+                setConstrains();
+                setBackground(new Color(190, 190, 190));
+                setBorder(BorderFactory.createRaisedBevelBorder());
+            }
+
+            public void addToComponent(ToolPanel tool) {
+                toolsPanel.addTool(tool);
+            }
+
+            public void removeFromComponent(ToolPanel tool) {
+                toolsPanel.removeTool(tool);
+            }
+
+            public void setConstrains() {
+                gbc = new GridBagConstraints();
+                gbc.fill = GridBagConstraints.BOTH;
+                gbc.insets = new Insets(2, 2, 2, 2);
+            }
+
+            public void setGridBagLayout(GridBagLayout gbl) {
+                setLayout(gbl);
+                gbl.rowWeights = new double[]{1};
+                gbl.columnWeights = new double[]{1};
+                this.gbl = gbl;
+            }
+        }
+        /*
+            END OF THE INNER CLASS ToolPanel
+        ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
     }
     /*
-        END OF INNER CLASS ToolsPanel
+        END OF INNER CLASS ToolComponent
     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 
     /**
-     * Basic calls to initialize the paint program
+     * Basic call to initialize the paint program
      */
     public static void initApp() {
         new PaintApp();
