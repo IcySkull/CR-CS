e10d2cda63ae155cb9645c5ceefcd608c6eb1373
Doblegor <diejorarr@gmail.com>
2022-10-25 13:56:02 -0500

    waitime lab done (might be wrong)

---
+++ 3K/Queues/WaitTime/src/WaitTime.java
@@ -0,0 +1,44 @@
+import java.util.*;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class WaitTime {
+    public static void main(String[] args) {
+        System.out.println(waitTime(new int[]{5,3,4}, 1)); // 12
+        System.out.println(waitTime(new int[]{10,2,3,3}, 2)); // 10
+        System.out.println(waitTime(new int[]{2,3,10}, 2)); // 12
+        
+        System.out.println(waitTime(new int[]{17, 2, 3, 6, 7, 8, 9, 4, 11, 22, 45, 1, 2, 3, 
+            4, 5, 6, 7, 53, 3, 2, 34, 42, 3, 3, 4, 4}, 6)); // 70
+
+        System.out.println(waitTime (new int[]{1, 2, 54, 12, 6, 7, 8, 4, 12, 17, 2, 3, 6, 7, 
+            8, 9, 12, 13, 11, 17, 22, 6, 5, 4, 3, 7, 8, 9, 12, 3, 4, 5, 6, 7, 15, 14, 1, 9, 8, 
+            7, 8, 9, 8, 44, 4, 11, 22, 45, 1, 2, 3, 9, 5, 4, 3, 2, 5, 6, 7, 8, 9, 5, 5, 4, 5, 6, 
+            7, 53, 3, 2, 34, 42, 3, 3, 4, 4}, 13)); // 92
+    }
+
+    public static int waitTime(int[] customers, int n) {
+        List<Integer> listCusto = Arrays.stream(customers).boxed().toList();
+        List<Integer> workingTime = listCusto.subList(0, n);
+        Queue<Integer> waitTime = new PriorityQueue<>(listCusto.subList(n, listCusto.size()));
+        int total = 0;
+        while (!workingTime.isEmpty()) {
+            int min = Collections.min(workingTime);
+            total += min;
+            workingTime = workingTime.stream()
+                .map(m -> {
+                    if (m == min) {
+                        if (!waitTime.isEmpty())
+                            return waitTime.poll();
+                        return 0;
+                    }
+                    return m - min;
+                })
+                .filter(m -> m != 0)
+                .collect(Collectors.toList());
+        }
+
+        return total;
+    }
+}
