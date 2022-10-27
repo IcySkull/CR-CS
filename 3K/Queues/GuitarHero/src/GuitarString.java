3d3ea73dd09ca4d8e3d8836d36f46aeba755c6d6
Doblegor <diejorarr@gmail.com>
2022-10-27 15:21:09 +0000

    ringbuffer lab

--- 3K/Queues/GuitarHero/src/GuitarString.java
+++ 3K/Queues/GuitarHero/src/GuitarString.java
@@ -1,3 +1,16 @@
 public class GuitarString {
+
+    public GuitarString(double cONCERT_A) {
+    }
+
+    public void pluck() {
+    }
+
+    public int sample() {
+        return 0;
+    }
+
+    public void tic() {
+    }
     
 }
--- 3K/Queues/GuitarHero/src/RingBuffer.java
+++ 3K/Queues/GuitarHero/src/RingBuffer.java
@@ -1,3 +1,87 @@
+import java.util.*;
+
 public class RingBuffer {
+    private double[] ary;
+    private int capacity;
+    private int size;
+    private int first;
+    private int last;
     
+    public RingBuffer(int capacity) {
+        this.capacity = capacity;
+        ary = new double[capacity];
+        first = 0;
+        last = 0;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public boolean isFull() {
+        return size == capacity;
+    }
+
+    public void enqueue(double x) {
+        if (isFull())
+            throw new IllegalStateException();
+        size++;
+        ary[last] = x;
+    }
+
+    public double dequeue() {
+        if (size == 0)
+            throw new NoSuchElementException();
+        double ret = ary[first];
+        incrementFirst();
+        return ret;
+    }
+
+    public double peek() {
+        if (size == 0)
+            throw new NoSuchElementException();
+        return ary[first];
+    }
+
+    public boolean incrementLast() {
+        last++;
+        if (last == capacity) {
+            last = 0;
+            return true;
+        }
+        return false;
+        
+    }
+
+    public int decrementLast(int last) {
+        last--;
+        if (last == -1) {
+            last = capacity-1;
+        }
+        return last;
+    }
+
+    public boolean incrementFirst() {
+        first++;
+        if (first == capacity) {
+            first = 0;
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        String out = "[";
+        while (first != decrementLast(last)) {
+            out += ary[first] + ", ";
+            incrementFirst();
+        }
+        out += ary[first] + "]";
+        return out;
+    }
 }
