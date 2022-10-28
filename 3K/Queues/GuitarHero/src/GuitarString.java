9d9b77acf40e5c7cc8051ffa74753146e7dc73f2
Doblegor <diejorarr@gmail.com>
2022-10-27 19:16:20 -0500

    GuitarString and RingBuffer done

--- 3K/Queues/GuitarHero/src/GuitarString.java
+++ 3K/Queues/GuitarHero/src/GuitarString.java
@@ -1,16 +1,36 @@
 public class GuitarString {
+    public RingBuffer ringBuffer;
+    public static final int SAMPLE_RATE = 44100; // 44100 samples per second
+    public static double ENERGY_DECAY_FACTOR = 0.994; // 0.994;
+    private int ticCount;
+    private int capacity;
 
-    public GuitarString(double cONCERT_A) {
+    public GuitarString(double frequency) {
+        capacity = (int) Math.round(SAMPLE_RATE / frequency);
+        pluck();
     }
 
     public void pluck() {
+        ringBuffer = new RingBuffer(capacity);
+        while (!ringBuffer.isFull()) {
+            ringBuffer.enqueue(Math.random()-0.5);
+        }
     }
 
-    public int sample() {
-        return 0;
+    public double sample() {
+        return ringBuffer.peek();
     }
 
     public void tic() {
+        ticCount++;
+        double first = ringBuffer.dequeue();
+        double second = ringBuffer.peek();
+        double average = (first + second) / 2;
+        ringBuffer.enqueue(average * ENERGY_DECAY_FACTOR);
+    }
+
+    public int time() {
+        return ticCount;
     }
     
 }
--- 3K/Queues/GuitarHero/src/RingBuffer.java
+++ 3K/Queues/GuitarHero/src/RingBuffer.java
@@ -1,17 +1,18 @@
 import java.util.*;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
 
-public class RingBuffer {
+public class RingBuffer implements Iterable<Double> {
     private double[] ary;
-    private int capacity;
     private int size;
     private int first;
     private int last;
     
     public RingBuffer(int capacity) {
-        this.capacity = capacity;
         ary = new double[capacity];
         first = 0;
         last = 0;
+        size = 0;
     }
 
     public int size() {
@@ -19,69 +20,106 @@ public class RingBuffer {
     }
 
     public boolean isEmpty() {
-        return size == 0;
+        return size() == 0;
     }
 
     public boolean isFull() {
-        return size == capacity;
+        return size() == ary.length;
     }
 
     public void enqueue(double x) {
         if (isFull())
             throw new IllegalStateException();
-        size++;
         ary[last] = x;
+        size++;
+        incrementLast();
     }
 
     public double dequeue() {
-        if (size == 0)
+        if (size() == 0)
             throw new NoSuchElementException();
-        double ret = ary[first];
         incrementFirst();
-        return ret;
+        size--;
+        return ary[decrementIndex(first)];
     }
 
     public double peek() {
-        if (size == 0)
+        if (size() == 0)
             throw new NoSuchElementException();
         return ary[first];
     }
 
-    public boolean incrementLast() {
-        last++;
-        if (last == capacity) {
-            last = 0;
-            return true;
-        }
-        return false;
-        
+    public int incrementIndex(int index) {
+        return (index + 1) % ary.length;
     }
 
-    public int decrementLast(int last) {
-        last--;
-        if (last == -1) {
-            last = capacity-1;
-        }
-        return last;
+    public int decrementIndex(int index) {
+        return (index - 1 + ary.length) % ary.length;
     }
 
-    public boolean incrementFirst() {
-        first++;
-        if (first == capacity) {
-            first = 0;
-            return true;
-        }
-        return false;
+    private void incrementLast() {
+        last = incrementIndex(last);
+    }
+
+    private void decrementLast() {
+        last = decrementIndex(last);
+    }
+
+    private void incrementFirst() {
+        first = incrementIndex(first);
+    }
+
+    private void decrementFirst() {
+        first = decrementIndex(first);
+    }
+
+    public Stream<Double> stream() {
+        return StreamSupport.stream(spliterator(), false);
     }
 
     @Override
     public String toString() {
-        String out = "[";
-        while (first != decrementLast(last)) {
-            out += ary[first] + ", ";
-            incrementFirst();
+        if (size() == 0)
+            return "[]";
+        Iterator<Double> it = iterator();
+        StringBuilder sb = new StringBuilder("[");
+        while (it.hasNext()) {
+            sb.append(it.next());
+            if (it.hasNext())
+                sb.append(", ");
+            else    
+                sb.append("]");
         }
-        out += ary[first] + "]";
-        return out;
+        return sb.toString();
+    }
+
+    @Override
+    public Iterator<Double> iterator() {
+        return new RingBufferIterator(first);
+    }
+
+private class RingBufferIterator implements Iterator<Double> {
+        private int index;
+        private int count;
+
+        public RingBufferIterator(int index) {
+            this.index = index;
+            count = 0;
+        }
+
+        @Override
+        public boolean hasNext() {
+            return count < size();
+        }
+
+        @Override
+        public Double next() {
+            if (!hasNext())
+                throw new NoSuchElementException();
+            double out = ary[index];
+            index = incrementIndex(index);
+            count++;
+            return out;
+        }
     }
 }
