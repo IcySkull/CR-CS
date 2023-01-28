61cce11d1d1c3b4d19dc97bda2604904f3859f67
Doblegor <43691965+Doblegor@users.noreply.github.com>
2023-01-28 23:09:11 +0000

    tries continue

---
+++ 3K/Trees/Tries/src/DictionaryBST.java
@@ -0,0 +1,34 @@
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class DictionaryBST implements Dictionary
+{
+   private TreeSet<String> dict;
+
+    public DictionaryBST (String[] words) {
+        dict = Arrays.stream(words).collect(Collectors.toSet());
+    }
+
+    /** Add this word to the dictionary.  Convert it to lowercase first
+     * for the assignment requirements.
+     * @param word The word to add
+     * @return true if the word was added to the dictionary
+     * (it wasn't already there). */
+    public boolean addWord(String word) {
+        return dict.add(word.toLowerCase());
+    }
+
+
+    /** Return the number of words in the dictionary */
+    public int size()
+    {
+        return dict.size();
+    }
+
+    /** Is this a word according to this dictionary? */
+    public boolean isWord(String s) {
+        return dict.contains(s.toLowerCase());
+    }
+
+}
---
+++ 3K/Trees/Tries/src/DictionaryLL.java
@@ -0,0 +1,41 @@
+
+import java.util.LinkedList;
+
+/**
+ * A class that implements the Dictionary interface using a LinkedList
+ *
+ */
+public class DictionaryLL implements Dictionary
+{
+
+	private LinkedList<String> dict;
+
+
+    public DictionaryLL(String[] words) {
+        dict = Arrays.stream(words).collect(Collectors.toList());
+    }
+
+
+    /** Add this word to the dictionary.  Convert it to lowercase first
+     * for the assignment requirements.
+     * @param word The word to add
+     * @return true if the word was added to the dictionary
+     * (it wasn't already there). */
+    public boolean addWord(String word) {
+        return dict.add(word.toLowerCase());
+    }
+
+
+    /** Return the number of words in the dictionary */
+    public int size()
+    {
+		return dict.size();
+    }
+
+    /** Is this a word according to this dictionary? */
+    public boolean isWord(String s) {
+		return dict.contains(s.toLowerCase());
+    }
+
+
+}
