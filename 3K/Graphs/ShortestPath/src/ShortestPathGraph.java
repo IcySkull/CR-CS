12cbf266ab0ffc1367788464c256d7bd90d0f194
Doblegor <43691965+Doblegor@users.noreply.github.com>
2023-02-23 16:06:30 +0000

    ShortestPath lab (2) completed

--- 3K/Graphs/ShortestPath/src/ShortestPathGraph.java
+++ 3K/Graphs/ShortestPath/src/ShortestPathGraph.java
@@ -9,9 +9,12 @@ import java.io.IOException;
 import java.util.Arrays;
 import java.util.Map;
 import java.util.TreeMap;
+import java.util.TreeSet;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import java.util.Scanner;
+import java.util.Set;
+
 import static java.lang.System.*;
 
 public class ShortestPathGraph
@@ -42,23 +45,25 @@ public class ShortestPathGraph
 	}
 
 	public int check(String first, String second) {
-		return check(first, second, new TreeMap<>(map));
+		return check(first, second, 0, new TreeSet<>());
 	}
 
-	public int check(String first, String second, Map<String, String> currMap) {
-		String connections = currMap.remove(first);
+	public int check(String first, String second, int steps, Set<String> visited) {
+		if (visited.contains(first))	
+			return 0;
+
+		String connections = map.get(first);
 		if (connections == null)
 			return 0;
 		if (connections.contains(second))
-			return 1;
-		int min = Arrays.stream(connections.split(""))
-			.mapToInt(s -> {
-				return check(s, second, new TreeMap<>(currMap));
-			})
-			.filter(i -> i > 0)
+			return steps+1;
+
+		visited.add(first);
+		return Arrays.stream(connections.split(""))
+			.mapToInt(edge -> check(edge, second, steps+1, new TreeSet<>(visited)))
+			.filter(steps1 -> steps1 > 0)
 			.min()
 			.orElse(0);
-		return 1 + min;
 	}
 
 
