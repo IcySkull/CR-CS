80b7ee88dff4f99902e7c5552e37b69d69a0a200
diejor <diejorarr@gmail.com>
2023-02-23 07:32:41 -0600

    shortest path started

--- 3K/Graphs/ShortestPath/src/ShortestPathGraph.java
+++ 3K/Graphs/ShortestPath/src/ShortestPathGraph.java
@@ -22,18 +22,43 @@ public class ShortestPathGraph
 
 	public ShortestPathGraph(String line) {
 		line = line.trim();
-		line = line + " " + (new StringBuilder(line)).reverse().toString();
-		map = Arrays.stream(line)
-			.collect(Collectors.toMap()
+		String reversed = (new StringBuilder(line)).reverse().toString();
+		line = line + " " + reversed;
+		map = Arrays.stream(line.split(" "))
+			.collect(
+				Collectors.toMap(
+					s -> ""+s.charAt(0),
+					s -> ""+s.charAt(1),
+					(String a, String b) -> a + b,
+					TreeMap::new
+				)
 		);
+		System.out.println(map);
 	}
 
 	public boolean contains(String letter)
 	{
-		return true;
+		return map.containsKey(letter);
 	}
 
-	public void check(String first, String second, int steps) {
+	public int check(String first, String second) {
+		return check(first, second, new TreeMap<>(map));
+	}
+
+	public int check(String first, String second, Map<String, String> currMap) {
+		String connections = currMap.remove(first);
+		if (connections == null)
+			return 0;
+		if (connections.contains(second))
+			return 1;
+		int min = Arrays.stream(connections.split(""))
+			.mapToInt(s -> {
+				return check(s, second, new TreeMap<>(currMap));
+			})
+			.filter(i -> i > 0)
+			.min()
+			.orElse(0);
+		return 1 + min;
 	}
 
 
--- 3K/Graphs/ShortestPath/src/ShortestPathGraphRunner.java
+++ 3K/Graphs/ShortestPath/src/ShortestPathGraphRunner.java
@@ -1,4 +1,4 @@
-//© A+ Computer Science  -  www.apluscompsci.com
+//ï¿½ A+ Computer Science  -  www.apluscompsci.com
 //Name -
 //Date -  
 //Class -
@@ -13,5 +13,21 @@ public class ShortestPathGraphRunner
 {
 	public static void main( String[] args ) throws IOException
 	{
+		Scanner file = new Scanner(new File("graph2.dat"));
+		int howManyTimes = file.nextInt();
+		file.nextLine();
+		for(int x=0; x<howManyTimes; x++)
+		{
+			ShortestPathGraph graph = new ShortestPathGraph(file.nextLine());
+			String connections = file.nextLine();
+			String node1 = String.valueOf(connections.charAt(0));
+			String node2 = String.valueOf(connections.charAt(1));
+			int shortest = graph.check(node1, node2);
+			if (shortest == 0)
+				System.out.println(node1 + " connects to " + node2 + " == no");
+			else
+				System.out.println(node1 + " connects to " + node2 + " == yes " + shortest);
+			System.out.println();
+		}
 	}
 }
