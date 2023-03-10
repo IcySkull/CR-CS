434ecfa6b96848e68283291071eea510ecadd141
diejor <diejorarr@gmail.com>
2023-03-10 00:50:27 -0600

    abstract graphs continue

--- 3K/Graphs/BFSDFS/src/AbstractGraph.java
+++ 3K/Graphs/BFSDFS/src/AbstractGraph.java
@@ -11,6 +11,11 @@ public abstract class AbstractGraph<V, W extends Number> {
     private Set<V> vertices;
     private Set<Edge> edges;
 
+    public AbstractGraph() {
+        this.vertices = new HashSet<>();
+        this.edges = new HashSet<>();
+    }
+
     public AbstractGraph(Set<V> vertices, Set<Edge> edges) {
         this.vertices = vertices;
         this.edges = edges;
@@ -22,6 +27,33 @@ public abstract class AbstractGraph<V, W extends Number> {
 
     public Set<Edge> getEdges() {
         return edges;
+    }
+
+    public void addVertices(V... vs) {
+        for (V v : vs)
+            vertices.add(v);
+    }
+
+    public void addVertex(V v) {
+        addVertices(v);
+    }
+
+    public void addEdges(Edge... es) {
+        for (Edge e : es)
+            edges.add(e);
+    }
+
+    public void addEdge(V v, V w, W weight) {
+        if (!vertices.contains(v))
+            throw new IllegalArgumentException("Vertex " + v + " is not in the graph");
+        if (!vertices.contains(w))
+            throw new IllegalArgumentException("Vertex " + w + " is not in the graph");
+        Edge e = new Edge(v, w, weight) {};
+        edges.add(e);
+    }
+
+    public void addEdge(V v, V w) {
+        addEdge(v, w, (W) Double.valueOf(1));
     }
 
     /**
@@ -67,6 +99,10 @@ public abstract class AbstractGraph<V, W extends Number> {
     }
 
     public <T extends Collection<V>> List<V> search (V start, V end, T traverse, Function<Edge, W> heuristic) {
+        if (!vertices.contains(start))
+            throw new IllegalArgumentException("Start vertex is not in the graph");
+        if (!vertices.contains(end))
+            throw new IllegalArgumentException("End vertex is not in the graph");
         Map<V, V> parent = new HashMap<>();
         Map<V, W> cost = new HashMap<>();
         traverse.add(start);
@@ -76,15 +112,8 @@ public abstract class AbstractGraph<V, W extends Number> {
             V v = traverse.iterator().next();
             traverse.remove(v);
 
-            if (v.equals(end)) {
-                List<V> path = new ArrayList<>();
-                while (v != null) {
-                    path.add(v);
-                    v = parent.get(v);
-                }
-                Collections.reverse(path);
-                return path;
-            }
+            if (v.equals(end))
+                return backtrace(parent, end);
 
             for (Edge e : incidentEdges(v)) {
                 V w = e.adj(v);
@@ -100,6 +129,20 @@ public abstract class AbstractGraph<V, W extends Number> {
         return null;
     }
 
+    /*
+     * Backtrace the path from start to end. Helper function for search.
+     */
+    public List<V> backtrace(Map<V, V> parent, V end) {
+        List<V> path = new ArrayList<>();
+        V v = end;
+        while (v != null) {
+            path.add(v);
+            v = parent.get(v);
+        }
+        Collections.reverse(path);
+        return path;
+    }
+
     public List<V> bfs(V start, V end) {
         return search(start, end, (Queue)(new LinkedList<>()), e -> (W) Double.valueOf(0));
     }
@@ -113,10 +156,14 @@ public abstract class AbstractGraph<V, W extends Number> {
         private V w;
         private W weight;
         
-        public Edge(W weight, V v, V w) {
+        public Edge(V v, V w, W weight) {
             this.v = v;
             this.w = w;
             this.weight = weight;
+        }
+
+        public Edge(V v, V w) {
+            this(v, w, (W) Double.valueOf(1));
         }
 
         /*
@@ -125,7 +172,7 @@ public abstract class AbstractGraph<V, W extends Number> {
          */
         public V adj(V u) {
             if (u.equals(v))
-                return this.v;
+                return this.w;
             else if (u.equals(w))
                 return this.v;
             throw new IllegalArgumentException("Vertex is not adjacent to this edge");
@@ -148,12 +195,14 @@ public abstract class AbstractGraph<V, W extends Number> {
     }
 
     public static void main(String[] args) {
-        Set<Integer> vertices = new HashSet<>();
-        Set<AbstractGraph<Integer, Double>.Edge> edges = new HashSet<>();
-        for (int i = 0; i < 5; i++) {
-            vertices.add(i);
-        }
-    }
 
-    
+        AbstractGraph<Integer, Number> graph = new AbstractGraph<Integer, Number>() {};
+        graph.addVertices(1, 2, 3);
+        graph.addEdge(1, 2);
+        graph.addEdge(3, 2);
+        graph.addEdge(1, 3);
+
+
+        System.out.println(graph.dfs(1, 4));
+    }
 }
